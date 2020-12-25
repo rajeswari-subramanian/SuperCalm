3b73df5e8736dbcbe76ccafe6a90f51a650ca32f
rajeswari-subramanian <rajilechrame@gmail.com>
2020-12-26 01:14:39 +0530

    video-autoPlay-added

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 function Admin() {
+
     return (
-        <div>
-            ADMIN
-        </div>
+        <>
+            <Navbar pad="-70px" color="blue" />            
+            <video width="100%" height="100%" loop autoPlay muted>
+                <source src='/Videos/calm.mp4' type='video/mp4' />
+            </video> 
+        </>
     )
 }
 
