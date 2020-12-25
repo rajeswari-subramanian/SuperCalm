1b5fb3c028dcb41bda1b1cddf12d18559abe882f
rajeswari-subramanian <rajilechrame@gmail.com>
2020-12-25 10:30:34 +0530

    created-userreducer

---
+++ src/Redux/User/reducer.js
@@ -0,0 +1,34 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT } from './actionTypes'
+import { saveData, loadData, clearData } from "../LocalStorage";
+
+const initStore = {
+    isUserAuth: loadData('isUserAuth') || false,
+    isError: false,
+};
+const userReducer = (state = initStore, { type, payload }) => {
+    switch (type) {
+        case USER_LOGIN_REQUEST:
+            return {
+                ...state,
+            };
+        case USER_LOGIN_SUCCESS:
+            saveData('isUserAuth', true);
+            return {
+                ...state,
+                isUserAuth: true,
+            };
+        case USER_LOGIN_FAILURE:
+            return {
+                ...state,
+                isError: true,
+            };
+        case USER_LOGOUT:
+            clearData();
+            return {
+                isUserAuth: false,
+            };
+        default:
+            return { ...state };
+    }
+};
+export default userReducer;
