1077658c362480e0a90eab4b7040f05c9a530a38
rajeswari-subramanian <rajilechrame@gmail.com>
2020-12-25 10:28:03 +0530

    user-admin-routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import LandingPage from "../Components/LandingPage";
+import Admin from '../Components/Admin'
+import User from '../Components/User'
 
 export default function Routes() {
     return (
@@ -8,6 +10,8 @@ export default function Routes() {
             <Router>
                 <Switch>                   
                     <Route path="/" render={(props) => <LandingPage {...props} />} />
+                    <Route path="/admin" render={(props) => <Admin {...props} />} />
+                    <Route path="/user" render={(props) => <User {...props} />} />
                     <Route>
                         <div style={{ color: "red" }}>Error 404</div>
                         <Link to="/">GO back Home</Link>
