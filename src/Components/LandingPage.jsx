22dd14380bee620ff332b36c355977d2aaf846a2
rajeswari-subramanian <rajilechrame@gmail.com>
2020-12-27 18:32:07 +0530

    footer-content-changed

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from './Footer'
 import styled from 'styled-components'
 
 const GetStartedButton = styled.button`
-    font-size: 18px;
-    outline: none;
-    border: 0px;
-    font-weight: bolder;
-    padding: 10px 105px 10px 105px;
-    color: white;
-    border-radius: 25px;
-    background: linear-gradient(lightblue, blue)
+font-size: 18px;
+outline: none;
+border: 0px;
+font-weight: bolder;
+padding: 10px 105px 10px 105px;
+color: white;
+border-radius: 25px;
+background: linear-gradient(lightblue, blue)
 `;
 const GetStartedContainer = styled.div`
 display: flex;
@@ -26,9 +26,14 @@ color: white;
 font-size:${props => props.font ? props.font : "29px"}
 `;
 export default function LandingPage() {
+    let [position, setPosition] = useState(window.pageYOffset)
+    setInterval(() => {
+        setPosition(window.pageYOffset)
+    }, 500)
+    //console.log(position)
     return (
         <>
-            <Navbar pad="80px" />
+            <Navbar pad="80px" position={position} />
             <div style={{ position: "relative" }}>
                 <img style={{
                     display: "block", marginTop: "-70px", filter: "brightness(0.5)"
