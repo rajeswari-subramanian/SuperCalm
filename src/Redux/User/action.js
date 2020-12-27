770f5c300e6b1951840e920119baf5704b47a616
rajeswari-subramanian <rajilechrame@gmail.com>
2020-12-27 22:04:11 +0530

    action-forweekname

--- src/Redux/User/action.js
+++ src/Redux/User/action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT, USER_STATS_SHOW } from './actionTypes'
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT, USER_STATS_SHOW, USER_WEEK } from './actionTypes'
 
 const userLoginRequest = () => {
     return { type: USER_LOGIN_REQUEST };
@@ -30,11 +30,18 @@ const userStatsShow = () => {
         type: USER_STATS_SHOW,
     };
 };
+const userWeek = (payload) => {
+    return {
+        type: USER_WEEK,
+        payload
+    };
+};
 export {
     userLoginRequest,
     userLoginSuccess,
     userLoginFailure,
     userLogout,
-    userStatsShow
+    userStatsShow,
+    userWeek
 };
 
